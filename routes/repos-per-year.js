237e540c0146796eae49814f25ce657bfd9c0e4d
Zeno Rocha <hi@zenorocha.com>
2015-04-30 22:15:29 -0700

    Excludes 2011 from the list

--- routes/repos-per-year.js
+++ routes/repos-per-year.js
@@ -3,39 +3,36 @@ var moment = require('moment');
 
 module.exports = function (req, res) {
   var template = {
-    "x_axis": { "type": "datetime", "labels": ["2011", "2012", "2013", "2014", "2015"]},
+    "x_axis": { "type": "datetime", "labels": ["2012", "2013", "2014", "2015"]},
     "series": [{"data": []}]
   };
 
   unirest
     .get('http://customelements-fetch.herokuapp.com/repos')
     .end(function (response) {
-      var eleven = 0, twelve = 0, thirteen = 0, fourteen = 0, fifteen = 0;
+      var twelve = 0, thirteen = 0, fourteen = 0, fifteen = 0;
 
       console.log('Total:', Object.keys(response.body).length);
 
       for (var entry in response.body) {
         var momentCreated = moment(response.body[entry].github.created_at);
 
-        if (momentCreated.isAfter('2015-01-01')) {
+        if (momentCreated.isAfter('2014-12-31')) {
           fifteen++;
           console.log('2015:', response.body[entry].github.full_name);
-        } else if (momentCreated.isAfter('2014-01-01')) {
+        } else if (momentCreated.isAfter('2013-12-31')) {
           fourteen++;
           console.log('2014:', response.body[entry].github.full_name);
-        } else if (momentCreated.isAfter('2013-01-01')) {
+        } else if (momentCreated.isAfter('2012-12-31')) {
           thirteen++;
           console.log('2013:', response.body[entry].github.full_name);
-        } else if (momentCreated.isAfter('2012-01-01')) {
+        } else if (momentCreated.isAfter('2011-12-31')) {
           twelve++;
           console.log('2012:', response.body[entry].github.full_name);
-        } else if (momentCreated.isAfter('2011-01-01')) {
-          eleven++;
-          console.log('2011:', response.body[entry].github.full_name);
         }
       }
 
-      template.series[0].data = [eleven, twelve, thirteen, fourteen, fifteen];
+      template.series[0].data = [twelve, thirteen, fourteen, fifteen];
       res.send(template);
     });
 };
